54427c521f14a2371122c2fed7aca690a06f3a55
Moretti Leonardo <leonardonabarro2003@gmail.com>
2023-10-18 11:43:46 -0300

    arrumando gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,13 +2,13 @@ const gulp = require('gulp')
 const sass = require('gulp-sass')(require('sass'))
 const imagemin = require('gulp-imagemin')
 
-function styles() {
+async function styles() {
     return gulp.src('./src/styles/*.scss')
         .pipe(sass({ outputStyle: 'compressed' }))
         .pipe(gulp.dest('./dist/css'))
 }
 
-function images() {
+async function images() {
     gulp.src('src/images/**/*')
 		.pipe(imagemin())
 		.pipe(gulp.dest('dist/images'))
